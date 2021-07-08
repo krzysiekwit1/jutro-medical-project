9df2eca46b876ba4a11195b1edfe39ee0bce8d70
Krzysztof Witkowski <krzysiekwit@gmail.com>
2021-07-08 12:14:12 +0200

    components/continents-countries-finished

--- src/components/Continents/Continents.tsx
+++ src/components/Continents/Continents.tsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { LOAD_CONTINENTS } from '../../GraphQL/Queries'
 import { Link } from 'react-router-dom'
+
+type Continent = {
+  code: string
+  name: string
+}
+
 function Continents() {
   const { error, data } = useQuery(LOAD_CONTINENTS)
 
@@ -20,13 +26,12 @@ function Continents() {
 
   return (
     <div className='grid grid-cols-4 gap-1.5 m-8 duration-1000 '>
-      {continents.map((continent, index) => (
-        <Link to='/continents:'>
+      {continents.map((continent: Continent) => (
+        <Link to={`/continents:${continent.code}`}>
           <div
             className='h-32 bg-blue-400 rounded-lg hover:bg-purple-700'
-            key={index}
+            key={continent.code}
           >
-            {console.log(continent.name)}
             <p className='pt-4 text-2xl pl-4'>{continent.name}</p>
             <p className='pt-2 text-xl pl-4'>{continent.code}</p>
           </div>
--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -2,19 +2,25 @@ import React, { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { useParams } from 'react-router-dom'
 import { LOAD_COUNTRIES } from '../../GraphQL/Queries'
+type Language = {
+  code: string
+  name: string
+  __typename: string
+}
 
 function Countries() {
-  const [countries, setCountries] = useState([])
   let { code } = useParams<{ code: string }>()
   code = code.substring(1)
-
   const { error, data } = useQuery(LOAD_COUNTRIES, {
     variables: { code },
   })
 
+  const [countries, setCountries] = useState<any[]>([])
+
   useEffect(() => {
     if (data) {
-      setCountries(data)
+      console.log(data.continent.countries)
+      setCountries(data.continent.countries)
     }
   }, [data])
 
@@ -22,7 +28,33 @@ function Countries() {
     console.log(error)
   }, [error])
 
-  return <div>{code}</div>
+  return (
+    <div className='grid grid-cols-3 gap-5 m-8 duration-1000'>
+      {countries.map((country) => (
+        <div
+          className='h-48 bg-blue-400 rounded-lg hover:bg-purple-700'
+          key={country.code}
+        >
+          <div className='flex'>
+            <p className='pt-4 text-2xl pl-4'>{country.name}</p>
+            <img
+              className='w-8 h-8 mt-5 ml-5'
+              alt='United States'
+              src={`http://purecatamphetamine.github.io/country-flag-icons/3x2/${country.code}.svg`}
+            />
+          </div>
+          <p className='pl-4 text-xl'>languages:</p>
+          <div className='pl-4 pt-2 flex'>
+            {country.languages.map((language: Language) => (
+              <p className='pr-4' key={language.code}>
+                {language.name}
+              </p>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Countries
