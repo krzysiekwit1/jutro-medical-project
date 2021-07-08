ac30fed7c5652fe76bbb8b796441225c2cf4c8b2
Krzysztof Witkowski <krzysiekwit@gmail.com>
2021-07-08 12:24:14 +0200

    component/Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
-  return <div></div>
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <Link to='/continents'>
+        <p className='text-5xl font-bold text-blue-500 hover:text-purple-500'>
+          GO TO CONTINENTS
+        </p>
+      </Link>
+    </div>
+  )
 }
 
 export default Home
